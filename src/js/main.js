9bc33372922c2b18faf6309b2adeced12541cb33
batogov <thesiamesecat19@gmail.com>
2017-03-16 00:51:09 +0300

    Переделал под JSON

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,36 @@
 
 (function() {
 
+
     var itemsContainer = document.querySelector(".content__items");
 
-    items.forEach(function (item) {
-       var element = getElementFromTemplate(item);
-       itemsContainer.appendChild(element);
-    });
+    getItems();
+
+
+    function renderItems (items) {
+        items.forEach(function (item) {
+            var element = getElementFromTemplate(item);
+            itemsContainer.appendChild(element);
+        });
+    }
+
+    
+    function getItems() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'data/items.json');
+
+        xhr.onload = function (event) {
+            var rawData = event.target.response;
+            var loadedItems = JSON.parse(rawData);
+
+            renderItems(loadedItems);
+        };
+
+        xhr.send();
+    }
+
 
     function getElementFromTemplate(data) {
-
         var element = document.createElement('div');
         element.classList.add("content-item");
 
@@ -18,5 +39,6 @@
 
         return element
     }
+
 
 })();
