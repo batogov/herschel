eda8f1957532d895f1425b112d5a01a2a95196d4
batogov <thesiamesecat19@gmail.com>
2017-03-16 22:17:54 +0300

    Фильтры + контент

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,62 @@
 
 (function() {
 
+    var items;
 
     var itemsContainer = document.querySelector(".content__items");
 
+    var activeCategoryFilter = 'category-all';
+    var filters = document.querySelectorAll("input[type=radio]");
+
+
+    for (var i = 0; i < filters.length; i++) {
+        filters[i].onclick = function (event) {
+
+            var filteredItems = items.slice(0);
+
+            var categoryValue = document.querySelector("input[name=category]:checked").value;
+            var colorValue = document.querySelector("input[name=color]:checked").value;
+            var priceValue = document.querySelector("input[name=price]:checked").value;
+
+            filteredItems = filteredItems.filter(function (item, i, arr) {
+
+                var categoryFlag = item.category === categoryValue || categoryValue === 'all';
+                var colorFlag = item.color === colorValue || colorValue === 'all';
+                var priceFlag;
+
+                if (priceValue === 'all') {
+                    priceFlag = true;
+                } else {
+                    if (priceValue === '100+') {
+                        priceFlag = parseInt(item.price) >= 100;
+                    } else {
+                        var priceMinMax = priceValue.split("-");
+                        priceFlag = parseInt(item.price) >= parseInt(priceMinMax[0]) && parseInt(item.price) <= parseInt(priceMinMax[1]);
+                    }
+                }
+
+                return categoryFlag && colorFlag && priceFlag;
+            });
+
+            renderItems(filteredItems);
+
+        }
+    }
+
+    
     getItems();
 
 
     function renderItems (items) {
+        itemsContainer.innerHTML = "";
+        var fragment = document.createDocumentFragment();
+
         items.forEach(function (item) {
             var element = getElementFromTemplate(item);
-            itemsContainer.appendChild(element);
+            fragment.appendChild(element);
         });
+
+        itemsContainer.appendChild(fragment);
     }
 
     
@@ -23,6 +68,7 @@
         xhr.onload = function (event) {
             var rawData = event.target.response;
             var loadedItems = JSON.parse(rawData);
+            items = loadedItems;
 
             renderItems(loadedItems);
         };
